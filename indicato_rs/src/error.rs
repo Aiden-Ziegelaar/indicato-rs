016f1c23651144b6f104203439df1056ab380195
Aiden-Ziegelaar <aidenziegelaar@outlook.com>
2024-03-10 14:15:56 +0800

    fix: Testing Errors

--- indicato_rs/src/error.rs
+++ indicato_rs/src/error.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum FinErrorType {
     DivideByZero,
     InvalidInput,
@@ -23,5 +23,20 @@ impl FinError {
 impl std::fmt::Display for FinError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "Error: {:?} - {}", self.error_type, self.message)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fin_error() {
+        let error = FinError::new(FinErrorType::InvalidInput, "Invalid input");
+        assert_eq!(error.error_type, FinErrorType::InvalidInput);
+        assert_eq!(error.message, "Invalid input");
+        assert_eq!(format!("{}", error), "Error: InvalidInput - Invalid input");
+        assert_eq!(format!("{:?}", error), "FinError { error_type: InvalidInput, message: \"Invalid input\" }");
     }
 }
